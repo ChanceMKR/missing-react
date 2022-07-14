4a2e0387f1918e0c275893fa83d73e65053048b5
ChanceMKR <87632730+ChanceMKR@users.noreply.github.com>
2022-07-15 02:39:37 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, {useState} from 'react';
+
 function App() {
+  const [fileImage, setFileImage] = useState();
+  const saveFileImage = (event)=>{
+    setFileImage(URL.createObjectURL(event.target.files[0]));
+  };
   return (
-    <div>
-      <h2>Missing</h2>
-      <input type="file" accept='.jpg, .jpeg, .png'/>
-    </div>
+    <>
+      <div>
+        <h2>Missing</h2>
+        <form onSubmit={event=>{
+          event.preventDefault();
+          window.location.href = "http://192.168.1.102:2000"
+        }}>
+          <img src={fileImage} width="216" height="288"/>
+          <p><input id="image" type="file" accept='image/*' onChange={saveFileImage}/></p>
+          <p><input type="submit"/></p>
+        </form>
+      </div>
+    </>
   );
 }
 
